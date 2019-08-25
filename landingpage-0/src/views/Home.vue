31b9b9c8702dd51980783fde8436b6ea29a647ca
Hackshirama <werneriasearch@gmail.com>
2019-08-24 23:09:03 -0400

    v0.0.1

--- landingpage-0/src/views/Home.vue
+++ landingpage-0/src/views/Home.vue
@@ -6,62 +6,57 @@
         <!--********************<SECCIÓN Nº0>********************-->
         <div class="banner_principal">
           <!--<SLIDERSHOW>-->
-            <div id="slider" class="slider elementos" v-on:scroll.passive="mediaQueries">
-              <div class="slide item vertical">
-              </div>
-                <div class="slide item vertical">
-              </div>
-              <div class="slide item vertical">
-              </div>
-              <div class="slide item vertical">
-              </div>
-            </div>            
-            <!--<SLIDERSHOW>-->
-            <div class="cont_logo">
-                <div class="container">
-                    <div class="col-xs-12 col-sm-3 col-md-4 col-lg-4 logo_aicad no-padding hidden-xs">
-                        <img src="img/home/aicad.png" class="img-responsive pull-left" alt="AICAD" title="AICAD" />
-                    </div>
-                    <div class="col-xs-12 col-sm-3 col-md-4 col-lg-4 logo_aicad no-padding visible-xs">
-                        <img src="img/home/aicad.png" class="img-responsive" alt="AICAD" title="AICAD" style="margin-top: 35px;"/>
+            
+              <div class="slideshow"></div>
+                <div class="content" v-on:scroll.passive="mediaQueries">
+                 <!--<SLIDERSHOW>-->
+                <div class="cont_logo">
+                    <div class="container">
+                        <div class="col-xs-12 col-sm-3 col-md-4 col-lg-4 logo_aicad no-padding hidden-xs">
+                            <img src="img/home/aicad.png" class="img-responsive pull-left" alt="AICAD" title="AICAD" />
+                        </div>
+                        <div class="col-xs-12 col-sm-3 col-md-4 col-lg-4 logo_aicad no-padding visible-xs">
+                            <img src="img/home/aicad.png" class="img-responsive" alt="AICAD" title="AICAD" style="margin-top: 75px;"/>
+                        </div>
                     </div>
                 </div>
-            </div>
-            
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 text-center">
+                            <h1>PRACTICAS EN EMPRESAS<br>Prácticas en empresas <strong><span>Madrid</span></strong></h1>
+                        </div>
+                    </div>
+                    <div class="row cont_circulos text-center">
+                        <ul class="list-inline">
+                            <li class="circulo"><p><strong>+10000</strong><br><span class="descripcion">ALUMNOS</span></p></li>
+                            <li class="circulo"><p><strong>+2500</strong><br><span class="descripcion">EMPRESAS</span></p></li>
+                            <li class="circulo"><p><strong>200</strong><br><span class="descripcion">UNIVERSIDADES<br />EN EUROPA</span></p></li>
+                            <li class="circulo"><p><strong>5</strong><br><span class="descripcion">CONTINENTES</span></p></li>
+                            <li class="circulo"><p><strong>120</strong><br><span class="descripcion">PAISES</span></p></li>
+                            <li class="circulo"><p><strong>15000</strong><br><span class="descripcion">PROGRAMAS</span></p></li>
+                        </ul>
+                    </div>
+                </div>
+                <!--********************<SECCIÓN GAMING>********************-->
+                <div class="seccion_gaming">
+                    <div class="container">
+                        <h2>¿Buscas oportunidades de formación con prácticas en empresas?</h2>
+                    </div>
+                </div>
+                <!--********************</SECCIÓN GAMING>********************-->
+                <div class="participar text-center">
+                    <div class="container">
+                        <a class="btn btn-default" href="/aicad/inscribete" title="">
+                            QUIERO MATRICULARME AHORA
+                        </a>
+                    </div>
+                </div>
+        <!--********************<SECCIÓN Nº1>********************-->   
+              </div>
         </div>     
         <!--********************</SECCIÓN Nº0>********************-->
-        <div class="container">
-          <div class="row">
-              <div class="col-xs-12 text-center">
-                  <h1>PRACTICAS EN EMPRESAS<br>Prácticas en empresas <strong><span>Madrid</span></strong></h1>
-              </div>
-          </div>
-          <div class="row cont_circulos text-center">
-              <ul class="list-inline">
-                  <li class="circulo"><p><strong>+10000</strong><br><span class="descripcion">ALUMNOS</span></p></li>
-                  <li class="circulo"><p><strong>+2500</strong><br><span class="descripcion">EMPRESAS</span></p></li>
-                  <li class="circulo"><p><strong>200</strong><br><span class="descripcion">UNIVERSIDADES<br />EN EUROPA</span></p></li>
-                  <li class="circulo"><p><strong>5</strong><br><span class="descripcion">CONTINENTES</span></p></li>
-                  <li class="circulo"><p><strong>120</strong><br><span class="descripcion">PAISES</span></p></li>
-                  <li class="circulo"><p><strong>15000</strong><br><span class="descripcion">PROGRAMAS</span></p></li>
-              </ul>
-          </div>
-      </div>
-      <!--********************<SECCIÓN GAMING>********************-->
-      <div class="seccion_gaming">
-          <div class="container">
-              <h2>¿Buscas oportunidades de formación con prácticas en empresas?</h2>
-          </div>
-      </div>
-      <!--********************</SECCIÓN GAMING>********************-->
-      <div class="participar text-center">
-          <div class="container">
-              <a class="btn btn-default" href="/aicad/inscribete" title="">
-                  QUIERO MATRICULARME AHORA
-              </a>
-          </div>
-      </div>
-        <!--********************<SECCIÓN Nº1>********************-->
+        
+      
         <!--********************<SECCIÓN: INSCRIBETE>********************-->
         <div class="inscribete_home">
           <!--********************<SECCIÓN: TIEMPO>********************-->
